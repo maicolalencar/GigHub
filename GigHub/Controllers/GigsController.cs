29b490c9d517932e37595bf2520f34dfbe81a2cf
Maicol Alencar <maicol.alencar@gmail.com>
2018-06-08 20:17:41 -0300

    Adicionar Add Gig FormView

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,25 @@
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var gigsForm = new GigFormViewModel()
+            {
+                Genres = _context.Genres
+            };
+            return View(gigsForm);
+        }
+    }
+}
