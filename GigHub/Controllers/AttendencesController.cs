149cb1f9bfb214a2ca12990ac0ec5a906faf8538
Maicol Alencar <maicol.alencar@gmail.com>
2018-06-13 19:49:11 -0300

    Inclusão Going button

---
+++ GigHub/Controllers/AttendencesController.cs
@@ -0,0 +1,32 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    public class AttendencesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendencesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IHttpActionResult Attend(AttendenceDto attendenceDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_context.Attendances.Any(a => a.GigId == attendenceDto.GigId && a.AttendeeId == userId))
+                return BadRequest("Comparecimento já estava confirmado");
+
+            var attendance = new Attendance() { AttendeeId = userId, GigId = attendenceDto.GigId };
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok("");
+        }
+    }
+}
